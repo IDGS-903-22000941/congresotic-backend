dcc828dc3adb7e3493ebdb303d87e593a6fc12c9
IDGS-803-22000941 <78941@alumnos.utleon.edu.mx>
2025-11-08 23:12:16 -0600

    Allow all CORS origins

--- Program.cs
+++ Program.cs
@@ -10,20 +10,16 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 // Configurar conexión a base de datos
-/* builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("CadenaSql"))); */
-
-// Cambia de SQL Server a PostgreSQL
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-// Configurar CORS
+// Configurar CORS - PERMITIR TODOS LOS ORÍGENES
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowReactApp",
         policy =>
         {
-            policy.WithOrigins("http://localhost:5173", "http://localhost:3000")
+            policy.AllowAnyOrigin()
                   .AllowAnyHeader()
                   .AllowAnyMethod();
         });
@@ -37,8 +33,6 @@ if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
-// app.UseHttpsRedirection(); // ← COMENTADO
 
 // Usar CORS
 app.UseCors("AllowReactApp");
