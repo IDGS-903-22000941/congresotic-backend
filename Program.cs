3d590605c41e2660f81d12c6052de261f9c50b3c
IDGS-803-22000941 <78941@alumnos.utleon.edu.mx>
2025-11-08 23:38:31 -0600

    Use DATABASE_URL environment variable

--- Program.cs
+++ Program.cs
@@ -10,8 +10,21 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 // Configurar conexión a base de datos
+// Leer DATABASE_URL o DefaultConnection
+var connectionString = Environment.GetEnvironmentVariable("DATABASE_URL")
+    ?? builder.Configuration.GetConnectionString("DefaultConnection");
+
+// Si viene de DATABASE_URL (formato PostgreSQL URL), convertir a formato Npgsql
+if (connectionString != null && connectionString.StartsWith("postgresql://"))
+{
+    var databaseUri = new Uri(connectionString);
+    var userInfo = databaseUri.UserInfo.Split(':');
+
+    connectionString = $"Host={databaseUri.Host};Port={databaseUri.Port};Database={databaseUri.AbsolutePath.TrimStart('/')};Username={userInfo[0]};Password={userInfo[1]};SSL Mode=Require;Trust Server Certificate=true";
+}
+
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseNpgsql(connectionString));
 
 // Configurar CORS - PERMITIR TODOS LOS ORÍGENES
 builder.Services.AddCors(options =>
